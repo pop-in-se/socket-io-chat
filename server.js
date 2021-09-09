ed67674bd17baa11ca3f9c12026cbbaa3543ec66
Victor Barraza <victor.barrazamerino@medieinstitutet.se>
2021-09-09 15:45:13 +0200

    Pågående
    On going för att lösa typing

--- server.js
+++ server.js
@@ -8,26 +8,50 @@ const port = 3000
 app.use(express.static('public'))
 
 const users = {}
+const typers = {}
 
 io.on('connection', (socket) => {
     console.log("User connected")
     
+    // Användare ansluter
+
     socket.on("new-user", userName => {
         users[socket.id] = userName
         socket.broadcast.emit('user-connected', userName)
     })
    
+    // skicka meddelande
+
     socket.on("message", incoming => {
+        delete typers[socket.id];   
         io.emit('message', incoming)
     })
     
+    //Användare skriver
+
+    socket.on('user typing', () => {
+        typers[socket.id] = 1;
+    
+        socket.broadcast.emit('user typing', {
+            user: users[socket.id].userName,
+            typers: Object.keys(typers).length
+        })
+    })
+
+    socket.on('user stopped typing', () => {
+        delete typers[socket.id];
+    
+        socket.broadcast.emit('user stopped typing', Object.keys(typers).length);
+    })
+    
+    // Användare lämnar
 
     socket.on("disconnect", () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
         /* console.log("User disconnected") */
     })
-})
+});
 
 
 http.listen(port, () => console.log("Listening on port " + port))
